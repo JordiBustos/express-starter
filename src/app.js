eb7bb611448a22ae0f7593b0fbef6ef9daf467c8
JordiBustos <jordi.bustos193771@alumnos.info.unlp.edu.ar>
2024-01-30 16:04:42 -0300

    added rate-limiter to all urls

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var limiter = require("./constants/limiter");
 require("dotenv").config();
 
 var indexRouter = require("./routes/index");
@@ -12,6 +13,7 @@ const db = require("./db");
 
 var app = express();
 
+app.use(limiter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
